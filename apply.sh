4fc8c5abfded475b70130c81fef8fa9d75b209f0
Aaron Miller <me@aaron-miller.me>
2021-11-12 22:21:18 -0500

    Use Let's Encrypt cert

--- apply.sh
+++ apply.sh
@@ -3,6 +3,7 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami 2>/dev/null
 helm repo add elastic https://helm.elastic.co 2>/dev/null
 helm repo add fusionauth https://fusionauth.github.io/charts 2>/dev/null
+helm repo add jetstack https://charts.jetstack.io
 
 # these are cluster-scoped, but would otherwise take a --namespace arg
 kubectl apply -f namespace.yaml
@@ -11,6 +12,7 @@ kubectl apply -f storage-class.yaml
 kubectl apply --namespace=fusionauth -f secret/postgres.yaml
 kubectl apply --namespace=fusionauth -f secret/fusionauth.yaml
 # kubectl apply --namespace=fusionauth -f secret/elasticsearch.yaml
+kubectl apply --namespace=fusionauth -f letsencrypt-issuer.yaml
 kubectl apply --namespace=fusionauth -f ingress.yaml
 
 helm install pg --namespace fusionauth \
@@ -29,3 +31,7 @@ helm install pg --namespace fusionauth \
 helm install fusionauth --namespace fusionauth \
      -f config/fusionauth.yaml \
      fusionauth/fusionauth
+
+helm install cert-manager --namespace fusionauth \
+     jetstack/cert-manager \
+     --set installCRDs=true
